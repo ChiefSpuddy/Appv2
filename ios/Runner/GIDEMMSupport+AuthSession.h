8fc88d59f57f1a8845b311aef7187392b680f755
ChiefSpuddy <sammaxwellmay@gmail.com>
2025-01-19 21:19:12 +0000

    Firebase Updates

--- ios/Runner/GIDEMMSupport+AuthSession.h
+++
@@ -1,8 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <GoogleSignIn/GoogleSignIn.h>
-#import <GTMAppAuth/GTMAppAuth.h>
-
-@interface GIDEMMSupport (AuthSession)
-@property (nonatomic, strong, nullable) GTMAppAuthFetcherAuthorization *authorization;
-@property (nonatomic, strong, nullable) OIDAuthState *authState;
-@end
