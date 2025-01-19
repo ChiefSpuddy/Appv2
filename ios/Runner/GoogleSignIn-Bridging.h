e8eec7b80ceabf04d50b2481735e74d53977b52b
ChiefSpuddy <sammaxwellmay@gmail.com>
2025-01-19 16:43:22 +0000

    Progress - Build Fixing Continues

--- ios/Runner/GoogleSignIn-Bridging.h
+++ ios/Runner/GoogleSignIn-Bridging.h
@@ -1,10 +1,16 @@
+#import <Foundation/Foundation.h>
 #import <GoogleSignIn/GoogleSignIn.h>
 #import <GTMAppAuth/GTMAppAuth.h>
+#import <GTMAppAuth/GTMAppAuthFetcherAuthorization.h>
+#import <GTMSessionFetcher/GTMSessionFetcher.h>
 #import <AppAuth/AppAuth.h>
 
-@protocol GTMAuthSessionDelegate;
-@class OIDAuthState;
+@protocol GTMAuthSessionDelegate <NSObject>
+@required
+- (void)didFinishWithAuth:(GTMAppAuthFetcherAuthorization *)authorization error:(NSError *)error;
+@end
 
 @interface GIDEMMSupport : NSObject <GTMAuthSessionDelegate>
 @property(nonatomic, strong) OIDAuthState *authState;
+@property(nonatomic, strong) GTMAppAuthFetcherAuthorization *authorization;
 @end
--- ios/Runner/Runner-Bridging-Header.h
+++ ios/Runner/Runner-Bridging-Header.h
@@ -1,2 +1,7 @@
+#ifndef Runner_Bridging_Header_h
+#define Runner_Bridging_Header_h
+
 #import "GeneratedPluginRegistrant.h"
 #import "GoogleSignIn-Bridging.h"
+
+#endif /* Runner_Bridging_Header_h */
