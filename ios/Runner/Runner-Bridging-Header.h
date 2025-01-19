3cf6c341c15ff814417c6a82115bbc880ed076d0
ChiefSpuddy <sammaxwellmay@gmail.com>
2025-01-19 17:20:01 +0000

    Progress - Build Fixing Continues

--- ios/Runner/Runner-Bridging-Header.h
+++ ios/Runner/Runner-Bridging-Header.h
@@ -2,19 +2,28 @@
 #define Runner_Bridging_Header_h
 
 #import "GeneratedPluginRegistrant.h"
-#import <GoogleSignIn/GoogleSignIn.h>
-#import <GTMAppAuth/GTMAppAuth.h>
-#import <GTMSessionFetcher/GTMSessionFetcher.h>
-#import <AppAuth/AppAuth.h>
+@import GoogleSignIn;
+@import GTMAppAuth;
+@import GTMSessionFetcher;
+@import AppAuth;
+
+@class GTMAppAuthFetcherAuthorization;
 
 @protocol GTMAuthSessionDelegate <NSObject>
-@required
-- (void)didFinishWithAuth:(GTMAppAuthFetcherAuthorization *)authorization error:(NSError *)error;
+- (void)didFinishWithAuth:(GTMAppAuthFetcherAuthorization *)authorization 
+                   error:(nullable NSError *)error;
 @end
+
+typedef NS_ENUM(NSInteger, GIDEMMErrorCode) {
+    GIDEMMErrorCodeUnknown = -1,
+    GIDEMMErrorCodeMissingConfiguration = 1,
+};
 
 @interface GIDEMMSupport : NSObject <GTMAuthSessionDelegate>
 @property(nonatomic, strong, nullable) OIDAuthState *authState;
 @property(nonatomic, strong, nullable) GTMAppAuthFetcherAuthorization *authorization;
+- (void)didFinishWithAuth:(nullable GTMAppAuthFetcherAuthorization *)authorization 
+                   error:(nullable NSError *)error;
 @end
 
 #endif /* Runner_Bridging_Header_h */
